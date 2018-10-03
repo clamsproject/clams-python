4b376eb62501a15953311700e936d302864ffaf1
Keigh Rim <keigh.rim@gmail.com>
2018-10-03 14:32:58 -0400

    prototype implementation of SDK

---
+++ README.md
@@ -0,0 +1,14 @@
+# CLAMS sdk for python 
+
+## installation: 
+```
+python setup.py install
+```
+This will install a packge `clams` to local python 
+
+## APIs
+
+
+`clams.ClamApp` : an abstract class for clams tool s
+
+`clams.Mmif` : serialization APIs
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name="clams", 
+        version="0.0.1",
+        author="Brandeis Lab for Linguistics and Computation", 
+        description="A collection of APIs to develop CLAMS tool for python", 
+        url="https://not.yet.available.io",
+        packages=setuptools.find_packages() 
+        )
