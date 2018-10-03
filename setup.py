4e4fa28872c4558aaa118ee17a3cc775c4665aa8
Keigh Rim <keigh.rim@gmail.com>
2018-10-03 15:13:21 -0400

    "clams" is now a pakcage, not a module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-        name="clams", 
+        name="clams-python-sdk", 
         version="0.0.1",
         author="Brandeis Lab for Linguistics and Computation", 
         description="A collection of APIs to develop CLAMS tool for python", 
