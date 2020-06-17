f7bed56e9131348b38b256441e40875f8f06c38f
Keigh Rim <keigh.rim@gmail.com>
2020-06-17 07:57:31 -0400

    prepping for pypi upload

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # CLAMS sdk for python 
 
+**NOTE** that this project is in pre-alpha and being actively developed. Nothing is guaranteed to reliably work for the moment and developer need to be very careful when using APIs implemented here. Please use [the issue track](../../issues) to report bugs and malfunctions, or send pull requests for even more contribution. 
+
+## CLAMS project
+[CLAMS project](https://www.clams.ai) aims at free and open-source software platform for computational analysis and metadata generation applications for multimedia material. 
+
 ## installation: 
+Package `clams-python` is distributed via the official pypi. Users are supposed to pip-install to get latest release. 
 ```
-python setup.py install
+pip install clams-python
 ```
 This will install a packge `clams` to local python 
 
 ## APIs
 
 
-`clams.ClamsApp` : an abstract class for clams tool s
-
-`clams.Mmif` : serialization APIs
+`clams.ClamsApp` : an abstract class for clams apps
--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 import setuptools
 
+with open('README.md') as readme:
+    long_desc = readme.read()
+
 setuptools.setup(
-        name="clams-python-sdk", 
-        version="0.0.1",
-        author="Brandeis Lab for Linguistics and Computation", 
-        description="A collection of APIs to develop CLAMS tool for python", 
-        url="https://not.yet.available.io",
-        packages=setuptools.find_packages() 
-        )
+    name="clams-python", 
+    version="0.0.1",
+    author="Brandeis Lab for Linguistics and Computation", 
+    author_email="admin@clams.al",
+    description="A collection of APIs to develop CLAMS app for python", 
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://www.clams.ai",
+    classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Framework :: Flask',
+    'Framework :: Pytest',
+    'Intended Audience :: Developers ',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3 :: Only',
+    ],
+    python_requires='>=3.6',
+    packages=setuptools.find_packages() 
+)
