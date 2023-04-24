e213152fd7ebc7ceec909431a2fbe685fb999039
Keigh Rim <keigh.rim@gmail.com>
2023-04-24 13:53:19 -0400

    ClamsApp.__init__ now tries to read ext. files to load appmetadata ...
    * added metadata.py to app template as an example

--- tests/test_clamsapp.py
+++ tests/test_clamsapp.py
@@ -60,21 +60,7 @@ class ExampleClamsApp(clams.app.ClamsApp):
     app_version = 'no-manual-version',
 
     def _appmetadata(self) -> Union[dict, AppMetadata]:
-        
-        metadata = AppMetadata(
-            name="Example CLAMS App for testing",
-            description="This app doesn't do anything",
-            app_license="MIT",
-            app_version=self.app_version,
-            identifier=f"https://apps.clams.ai/example",
-            output=[{'@type': AnnotationTypes.TimeFrame}],
-            dependencies=['clams-python==develop-ver', 'mmif-pyhon==0.0.999'],
-            url="https://fakegithub.com/some/repository"
-        )
-        metadata.add_input(DocumentTypes.AudioDocument)
-        metadata.add_parameter(name='raise_error', description='force raise a ValueError',
-                               type='boolean', default='false')
-        return metadata
+        pass
     
     def _annotate(self, mmif, **kwargs):
         if type(mmif) is not Mmif:
